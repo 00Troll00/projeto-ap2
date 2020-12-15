62ce0322ca0404437293f90888d3b75f93a4a10b
Alan Bonfim <63169838+00Troll00@users.noreply.github.com>
2020-12-15 15:25:29 -0300

    Add files via upload
    Pacote com as classes

---
+++ model/CarroNovo.java
@@ -0,0 +1,23 @@
+package projeto;
+public class CarroNovo extends Carro {
+    private int qntEstoque;
+    
+
+    
+    public CarroNovo(int ano, String modelo, String marca, double preco, int qntEstoque){
+        super(ano,modelo, marca, preco);
+        this.qntEstoque=qntEstoque;        
+    }
+
+
+    
+    public void setQntEstoque(int qntEstoque){
+        this.qntEstoque=qntEstoque;
+    }
+    
+    public int getQntEstoque(){
+        return qntEstoque;
+    }
+
+   
+}
---
+++ model/QuemSomos.java
@@ -0,0 +1,12 @@
+package projeto;
+public class QuemSomos {
+ private String quemSomos;
+ 
+ public String imprimir(){
+   return quemSomos; 
+     
+ }
+ 
+ 
+ 
+}
